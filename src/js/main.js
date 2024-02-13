bd2055f9075b80c1bfbd272d3fde449249a3dac1
Moa Berglund <moa.21@hotmail.com>
2024-02-13 10:31:48 +0100

    js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
-
-const url = "https://pokeapi.co/api/v2/pokemon/";
+let fetchedData = [];
+const url = "https://pokeapi.co/api/v2/pokemon/1/";
 
 //hämta data
 async function fetchAPI() {
@@ -16,4 +16,20 @@ async function fetchAPI() {
     catch (error) {
         document.getElementById("error").innerHTML = "<p>Något gick fel...</p>"
     }
+}
+
+fetchAPI().then(() => {
+    //när fetchAPI är klar anropas denna
+    //för att visa data
+    displayData();
+})
+
+
+function displayData() {
+    const pokeList = document.getElementById("poke");
+    pokeList.innerHTML = "";
+    fetchedData.array.forEach((item) => {
+        pokeList.innerHTML += `<div class="pokemon">${item}</div>`
+        
+    });
 }
